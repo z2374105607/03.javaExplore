c7894d594a6127e28b444780971789b2f3f02c21
张凯 <237405607@qq.com>
2021-10-22 19:41:27 +0800

    kaishi

---
+++ Spring-Boot-Session-Example/src/main/java/com/sxkg/SpringBootSessionApplication.java
@@ -0,0 +1,11 @@
+package com.sxkg;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSessionApplication.class, args);
+	}
+}
